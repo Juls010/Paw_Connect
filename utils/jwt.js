ba60f5f19f24a9602e30871235eaca3894ccc72b
Juls00 <juliagimena00@alumnos.cei.es>
2026-05-19 09:49:51 +0200

    -modificacion de jwt

--- utils/jwt.js
+++ utils/jwt.js
@@ -8,14 +8,14 @@ function generateTokens(user) {
         roles: user.roles       
     };
 
-    const access = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY 
+    const access = jwt.sign(payload, process.env.JWT_SECRET || 'test_secret', {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY || '15m'
     });
 
     const refresh = jwt.sign(
         { sub: user._id }, 
-        process.env.JWT_REFRESH_SECRET, 
-        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+        process.env.JWT_REFRESH_SECRET || 'test_refresh_secret', 
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY || '7d' }
     );
 
     return { access, refresh }; 
